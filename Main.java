1535b0f58a175e033dab164ae2f81439cb6cbe47
SaeKwang Kim <dnwlgks@naver.com>
2017-04-18 18:41:21 +0900

    commit3

--- Main.java
+++ Main.java
@@ -6,11 +6,7 @@ public class Main {
 		SimpleCalculator calc = new SimpleCalculator();
 		calc.add(10,20);
 		System.out.println(calc.getResult());
-		SimpleCalculator calc2 = new SimpleCalculator();
-		calc2.sub(10,20);
-		calc2.dec(3);
-		System.out.println(calc2.getResult());
-		
+
 	}
 
 }
